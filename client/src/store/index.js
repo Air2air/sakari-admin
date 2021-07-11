57a5213d7b98f7fdfc9bcfd7905c49f427d96031
Air2air <todd@dunningmarketing.com>
2021-07-11 00:23:25 -0700

    All pages

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import counter from "../pages/Home/counterReducer"
 import alerts from "../pages/Alerts/alertsReducer";
-import settings from "../pages/Setting/settingsReducer";
+import settings from "../pages/Settings/settingsReducer";
 
 
 
