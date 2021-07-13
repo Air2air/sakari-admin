49a294e4d82ba8f7d16659e902103d21c9bf52e6
Air2air <todd@dunningmarketing.com>
2021-07-12 17:18:03 -0700

    Predeploy

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import counter from "../pages/Home/counterReducer"
-import alerts from "../pages/Alerts/alertsReducer";
 import settings from "../pages/Settings/settingsReducer";
-
 
 
 export default configureStore({
   reducer: {
-    alerts: alerts,
     settings: settings,
     counter: counter,
   },
