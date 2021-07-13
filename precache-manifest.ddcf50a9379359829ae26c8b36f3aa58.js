391436686b2bd9911199a1a0e83e300644eeee12
Todd Dunning <Air2air@users.noreply.github.com>
2021-07-12 18:03:34 -0700

    Add files via upload

---
+++ precache-manifest.ddcf50a9379359829ae26c8b36f3aa58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de2e137a9dbe433861c9ee1a8276c87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6595601ae633b836af4",
+    "url": "./static/js/2.13c310c8.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "./static/js/2.13c310c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f300df502b376592a4db",
+    "url": "./static/js/main.40a024bf.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
